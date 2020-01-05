2a2aa190b8ae9d610340fcb434efedd0641dfff8
Alex Langshur <adlangshur@gmail.com>
2020-01-05 01:18:37 -0600

    Implemented up,down,left,right gesture handling

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, StatusBar, Text, Button, StyleSheet } from 'react-native';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 
 class Game extends Component {
     constructor(props) {
@@ -15,27 +16,61 @@ class Game extends Component {
 
     render = () => {
         return (
-            <View style={styles.container}>
-                <Text style={styles.scoreText}>{this.state.score}</Text>
+            <GestureRecognizer
+                onSwipeUp={this._onSwipeUp}
+                onSwipeDown={this._onSwipeDown}
+                onSwipeLeft={this._onSwipeLeft}
+                onSwipeRight={this._onSwipeRight}
+                config={{
+                    velocityThreshold: 0.3, // TODO: TWEAK
+                    directionalOffsetThreshold: 80 // TODO: TWEAK
+                }}
+                style={{
+                    flex: 1,
+                    backgroundColor: this.state.backgroundColor
+                }}
+            >
+                <View style={styles.container}>
+                    <Text style={styles.scoreText}>{this.state.score}</Text>
 
-                <Button 
-                    title='Game Over'
-                    onPress={this._handleGameOver}
-                />
-            </View>
+                    <Button 
+                        title='Game Over'
+                        onPress={this._handleGameOver}
+                    />
+                </View>
+            </GestureRecognizer>
         );
     }
 
     _runGamePage = () => {
         console.log('Game page.');
         StatusBar.setHidden(true);
+
+
+
+
+
+
+        // this.props.navigation.replace('End', {
+        //     score: this.state.score
+        // });
     }
 
-    _handleGameOver = () => {    
-        this.props.navigation.replace('End', {
-            score: this.state.score
-        });
-    };
+    _onSwipeUp() {
+        console.log('You swiped up!');
+    }
+     
+    _onSwipeDown() {
+        console.log('You swiped down!');
+    }
+    
+    _onSwipeLeft() {
+        console.log('You swiped left!');
+    }
+    
+    _onSwipeRight() {
+        console.log('You swiped right!');
+    }
 }
 
 const styles = StyleSheet.create({
